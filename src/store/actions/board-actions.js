a234ad55c7aa9b7535c4e57eed168129f856e1c5
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-10-13 09:50:30 +0300

    feat(board): add board actions to the global state store

---
+++ src/store/actions/board-actions.js
@@ -0,0 +1,99 @@
+import {
+  SET_BOARDS,
+  ADD_BOARD,
+  UPDATE_BOARD,
+  REMOVE_BOARD,
+  SET_BOARD,
+  SET_LOADING,
+  SET_ERROR,
+} from "../reducers/board-reducer";
+
+import { store } from "../store";
+import { boardService } from "../../services/board/board-service-local";
+
+export async function loadBoards() {
+  try {
+    store.dispatch(setLoading(true));
+    const boards = await boardService.query();
+    store.dispatch(setBoards(boards));
+  } catch (error) {
+    store.dispatch(setError(`Error loading boards: ${error.message}`));
+  } finally {
+    store.dispatch(setLoading(false));
+  }
+}
+
+export async function loadBoard(boardId) {
+  try {
+    store.dispatch(setLoading(true));
+    const board = await boardService.getById(boardId);
+    store.dispatch(setBoard(board));
+  } catch (error) {
+    store.dispatch(setError(`Error loading board: ${error.message}`));
+  } finally {
+    store.dispatch(setLoading(false));
+  }
+}
+
+export async function createBoard(board) {
+  try {
+    const newBoard = await boardService.updateBoardWithActivity(board, {
+      key: "created board",
+      value: "",
+    });
+    store.dispatch(addBoard(newBoard));
+    return newBoard;
+  } catch (error) {
+    store.dispatch(setError(`Error creating board: ${error.message}`));
+    throw error;
+  }
+}
+
+export async function updateBoard(board) {
+  try {
+    const updatedBoard = await boardService.updateBoardWithActivity(board);
+    store.dispatch(editBoard(updatedBoard));
+    return updatedBoard;
+  } catch (error) {
+    store.dispatch(setError(`Error updating board: ${error.message}`));
+    throw error;
+  }
+}
+
+export async function removeBoard(boardId) {
+  try {
+    await boardService.remove(boardId);
+    store.dispatch(deleteBoard(boardId));
+  } catch (error) {
+    store.dispatch(setError(`Error removing board: ${error.message}`));
+    throw error;
+  }
+}
+
+export function setBoards(boards) {
+  return { type: SET_BOARDS, payload: boards };
+}
+
+export function addBoard(board) {
+  return { type: ADD_BOARD, payload: board };
+}
+
+export function editBoard(board) {
+  return { type: UPDATE_BOARD, payload: board };
+}
+
+export function deleteBoard(boardId) {
+  return { type: REMOVE_BOARD, payload: boardId };
+}
+
+export function setBoard(board) {
+  return { type: SET_BOARD, payload: board };
+}
+
+export function setLoading(isLoading) {
+  return { type: SET_LOADING, payload: isLoading };
+}
+
+export function setError(error) {
+  return { type: SET_ERROR, payload: error };
+}
