88fd2a98ebf86c9dda52cffb4a737c9d7597903f
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-10-17 16:29:52 +0300

    fix: change piece of state from user to currentUser

--- src/pages/AdminIndex.jsx
+++ src/pages/AdminIndex.jsx
@@ -6,8 +6,8 @@ import { useNavigate } from "react-router";
 export function AdminIndex() {
   const navigate = useNavigate();
 
-  const user = useSelector(storeState => storeState.userModule.user);
-  const users = useSelector(storeState => storeState.userModule.users);
+  const user = useSelector(storeState => storeState.users.currentUser);
+  const users = useSelector(storeState => storeState.users.currentUsers);
   const isLoading = useSelector(storeState => storeState.userModule.isLoading);
 
   useEffect(() => {
