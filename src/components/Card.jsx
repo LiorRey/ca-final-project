2768e6b9bd062dd0437cbdc61c1a9b347d4bf494
Vlad Grey <Vlad9rey@gmail.com>
2025-10-19 20:46:42 +0300

    feat: Added edit card from the card itself

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,7 @@ import {
   NotesRounded,
   RemoveRedEyeOutlined,
   DriveFileRenameOutline,
+  SaveSharp,
 } from "@mui/icons-material";
 
 import CardPopover from "./CardPopover";
@@ -12,6 +13,7 @@ import { useState } from "react";
 
 export function Card({ card, onRemoveCard, onUpdateCard }) {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [title, setTitle] = useState(card.title);
 
   function handleClick(e) {
     setAnchorEl(e.currentTarget);
@@ -19,9 +21,13 @@ export function Card({ card, onRemoveCard, onUpdateCard }) {
 
   function handleClose() {
     setAnchorEl(null);
+    setTitle(card.title);
   }
 
-  function handleSave() {}
+  function handleSave() {
+    card.title = title;
+    handleClose();
+  }
 
   function handleDelete() {
     onRemoveCard(card.id);
@@ -33,47 +39,72 @@ export function Card({ card, onRemoveCard, onUpdateCard }) {
 
   return (
     <section className="card-container">
-      <Box
-        className={`${open ? "floating-card-content" : "card-content"}`}
-        sx={open ? { zIndex: theme => theme.zIndex.modal + 1 } : {}}
-      >
-        {card.labels.length > 0 && (
-          <div className="card-labels">
-            {card.labels.map(label => (
-              <div
-                key={`${card.id}-${label.name}`}
-                className={`card-label ${label.color}`}
-              >
-                {label.name}
-              </div>
-            ))}
-          </div>
-        )}
-        <h3 className={`card-title ${card.labels.length === 0 ? "mr-2" : ""}`}>
-          {card.title}
-        </h3>
-        {open ? (
+      {open ? (
+        <Box
+          className="floating-card-content"
+          sx={{ zIndex: theme => theme.zIndex.modal + 2 }}
+        >
+          {card.labels.length > 0 && (
+            <div className="card-labels">
+              {card.labels.map(label => (
+                <div
+                  key={`${card.id}-${label.name}`}
+                  className={`card-label ${label.color}`}
+                >
+                  {label.name}
+                </div>
+              ))}
+            </div>
+          )}
+          <textarea
+            className="card-title-input"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
           <div className="card-footer">
             <div className="empty-card-footer" />
           </div>
-        ) : (
+          <button className="icon-button card-edit-button" />
+          <button onClick={handleSave} className="icon-button card-save-button">
+            Save
+          </button>
+        </Box>
+      ) : (
+        <Box className="card-content">
+          {card.labels.length > 0 && (
+            <div className="card-labels">
+              {card.labels.map(label => (
+                <div
+                  key={`${card.id}-${label.name}`}
+                  className={`card-label ${label.color}`}
+                >
+                  {label.name}
+                </div>
+              ))}
+            </div>
+          )}
+
+          <h3
+            className={`card-title ${card.labels.length === 0 ? "mr-2" : ""}`}
+          >
+            {card.title}
+          </h3>
+
           <div className="card-footer">
             <RemoveRedEyeOutlined />
             <ChatRounded />
             <NotesRounded />
           </div>
-        )}
-        <button
-          onClick={handleClick}
-          aria-describedby={id}
-          aria-label="Edit card"
-          className="icon-button card-edit-button"
-        >
-          <DriveFileRenameOutline />
-        </button>
-      </Box>
-
-      {/* We could send the card object to the popover to edit the card which would be more efficient */}
+          <button
+            onClick={handleClick}
+            aria-describedby={id}
+            aria-label="Edit card"
+            className="icon-button card-edit-button"
+          >
+            <DriveFileRenameOutline />
+          </button>
+        </Box>
+      )}
       <CardPopover
         open={open}
         anchorEl={anchorEl}
--- src/components/CardPopover.jsx
+++ src/components/CardPopover.jsx
@@ -21,6 +21,8 @@ export default function CardPopover({
     >
       <Popover
         disableScrollLock
+        disableEnforceFocus
+        disableAutoFocus
         className="card-popover"
         id={id}
         open={open}
