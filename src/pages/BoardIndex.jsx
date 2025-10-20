f47ee7b4e21bfa38f22e925447eda6f684e976a2
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-10-20 14:49:15 +0300

    fix: temporarily navigate from the index to the first board
    This is used to to keep the index page functional while allowing to work
    with a "default" board. This is a temporary solution until the full board
    selection UI is implemented.

--- src/pages/BoardIndex.jsx
+++ src/pages/BoardIndex.jsx
@@ -1,12 +1,23 @@
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { BoardDetails } from "./BoardDetails";
 import { boardService } from "../services/board";
-import { loadBoard } from "../store/actions/board-actions";
+import { loadBoard, loadBoards } from "../store/actions/board-actions";
 
 export function BoardIndex() {
+  const boards = useSelector(state => state.boards.boards);
+  const navigate = useNavigate();
+
   useEffect(() => {
-    fetchBoard();
+    loadBoards();
   }, []);
+
+  useEffect(() => {
+    if (boards.length === 0) return;
+
+    navigate(`/board/${boards[0]._id}`);
+  }, [boards]);
 
   async function fetchBoard() {
     const boards = await boardService.query();
