4d3afd4c8b7fdeeb6d4f47a87838ec0fcb117238
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-10-21 15:31:46 +0300

    feat(filter-by-state): add filterBy state and actions to the global state

--- src/store/reducers/board-reducer.js
+++ src/store/reducers/board-reducer.js
@@ -5,12 +5,19 @@ export const ADD_BOARD = "ADD_BOARD";
 export const UPDATE_BOARD = "UPDATE_BOARD";
 export const SET_LOADING = "boards/SET_LOADING";
 export const SET_ERROR = "boards/SET_ERROR";
+export const SET_FILTERS = "boards/SET_FILTERS";
+export const CLEAR_ALL_FILTERS = "boards/CLEAR_ALL_FILTERS";
 
 const initialState = {
   boards: [],
   board: null,
   isLoading: false,
   error: null,
+  filterBy: {
+    labels: [],
+    title: "",
+    includeNoLabels: false,
+  },
 };
 
 export function boardReducer(state = initialState, action) {
@@ -30,6 +37,16 @@ export function boardReducer(state = initialState, action) {
       return { ...state, isLoading: action.payload };
     case SET_ERROR:
       return { ...state, error: action.payload };
+    case SET_FILTERS:
+      return {
+        ...state,
+        filterBy: { ...state.filterBy, ...action.payload },
+      };
+    case CLEAR_ALL_FILTERS:
+      return {
+        ...state,
+        filterBy: getDefaultFilter(),
+      };
     default:
       return state;
   }
