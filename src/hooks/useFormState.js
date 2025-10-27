0cc072de8f67758547356ab0b713a887960a01f7
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-10-27 17:15:09 +0200

    feat: add a custom hook for handling form state

--- src/hooks/useFormState.js
+++ src/hooks/useFormState.js
@@ -3,20 +3,24 @@ import { useState } from "react";
 export function useFormState(initialValues = {}) {
   const [values, setValues] = useState(initialValues);
 
-  const handleChange = e => {
+  function handleChange(e) {
     const { name, value, type, checked } = e.target;
-    setValues(prev => ({
-      ...prev,
-      [name]:
-        type === "checkbox"
-          ? checked
-          : type === "number"
-          ? Number(value)
-          : value,
-    }));
-  };
+    setValues(function (prev) {
+      return {
+        ...prev,
+        [name]:
+          type === "checkbox"
+            ? checked
+            : type === "number"
+            ? Number(value)
+            : value,
+      };
+    });
+  }
 
-  const resetForm = () => setValues(initialValues);
+  function resetForm() {
+    setValues(initialValues);
+  }
 
   return { values, handleChange, setValues, resetForm };
 }
