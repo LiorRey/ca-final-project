6c61a087b2f89b49d4465e25f6b615c28f6c1817
Vlad Grey <Vlad9rey@gmail.com>
2025-11-02 14:27:57 +0200

    Fixed the bug of the edit card

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -39,15 +39,6 @@ export function List({
     });
   }
 
-  async function onRemoveCard(cardId) {
-    await onRemoveList(list.id);
-  }
-
-  async function onUpdateCard(card) {
-    setCards(prev => [...prev, card]);
-    await onUpdateList(list, { cards });
-  }
-
   function handleMoreClick(event) {
     setAnchorEl(event.currentTarget);
   }
@@ -132,10 +123,9 @@ export function List({
               <li key={card.id}>
                 <Card
                   card={card}
+                  listId={list.id}
                   labels={cardLabels}
                   onClickCard={card => handleOpenModal(card)}
-                  onRemoveCard={onRemoveCard}
-                  onUpdateCard={onUpdateCard}
                   labelsIsOpen={labelsIsOpen}
                   setLabelsIsOpen={setLabelsIsOpen}
                 />
