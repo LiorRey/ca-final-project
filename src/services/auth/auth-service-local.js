2fdda5208265229131e1ed1e662c2d38df66f571
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-11-10 18:58:00 +0200

    feat(auth-service): add signup functionality to both local and remote services

--- src/services/auth/auth-service-local.js
+++ src/services/auth/auth-service-local.js
@@ -1,1 +1,29 @@
-export const authService = {};
+import { storageService } from "../async-storage-service";
+
+export const authService = {
+  signup,
+  setCurrentUserInSession,
+  getCurrentUserFromSession,
+};
+
+const USERS_STORAGE_KEY = "userDB";
+const CURRENT_USER_SESSION_KEY = "currentUser";
+
+export async function signup(userData) {
+  try {
+    const user = await storageService.post(USERS_STORAGE_KEY, userData);
+    setCurrentUserInSession(user);
+    return user;
+  } catch (error) {
+    console.error("Error during signup:", error);
+    throw error;
+  }
+}
+
+export function setCurrentUserInSession(user) {
+  sessionStorage.setItem(CURRENT_USER_SESSION_KEY, JSON.stringify(user));
+}
+export function getCurrentUserFromSession() {
+  const user = sessionStorage.getItem(CURRENT_USER_SESSION_KEY);
+  return user ? JSON.parse(user) : null;
+}
