109a5e23642584daea4742f787564b52089db21d
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-11-10 18:58:00 +0200

    feat(auth-service): add signup functionality to both local and remote services

--- src/services/auth/auth-service-remote.js
+++ src/services/auth/auth-service-remote.js
@@ -1,1 +1,11 @@
-export const authService = {};
+export const authService = {
+  signup,
+  setCurrentUserInSession,
+  getCurrentUserFromSession,
+};
+
+export async function signup(userData) {}
+
+export function setCurrentUserInSession(user) {}
+
+export function getCurrentUserFromSession() {}
