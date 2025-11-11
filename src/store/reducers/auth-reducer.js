09e9bdb4c548bb04057ccf6cfdf829fd3082c774
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-11-11 19:11:05 +0200

    feat(state): add login actions and reducer case

--- src/store/reducers/auth-reducer.js
+++ src/store/reducers/auth-reducer.js
@@ -1,6 +1,7 @@
 import { createAsyncActionTypes, createAsyncHandlers } from "../utils";
 
 export const SIGNUP = createAsyncActionTypes("SIGNUP");
+export const LOGIN = createAsyncActionTypes("LOGIN");
 
 const initialState = {
   currentUser: null,
@@ -13,6 +14,12 @@ const handlers = {
   [SIGNUP.SUCCESS]: (state, action) => ({
     ...state,
     loading: { ...state.loading, [SIGNUP.KEY]: false },
+    currentUser: action.payload,
+  }),
+  ...createAsyncHandlers(LOGIN, LOGIN.KEY),
+  [LOGIN.SUCCESS]: (state, action) => ({
+    ...state,
+    loading: { ...state.loading, [LOGIN.KEY]: false },
     currentUser: action.payload,
   }),
 };
