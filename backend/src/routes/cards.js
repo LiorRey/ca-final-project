60b763912b4a88ac927c54f6a7937e0563e15417
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-11-29 10:49:51 +0200

    fix: protect create/delete/update with authentication

--- backend/src/routes/cards.js
+++ backend/src/routes/cards.js
@@ -8,16 +8,23 @@ import {
   getCardsByLabel,
   getCardsByAssignedUser,
 } from "../controllers/card-controller.js";
+import { authenticate } from "../middleware/authenticate.js";
 
 const router = express.Router();
 
-router.post("/", createCard);
 router.get("/", getAllCards);
 router.get("/:id", getCardById);
-router.put("/:id", updateCard);
-router.delete("/:id", deleteCard);
 
-router.get("/label/:labelId", getCardsByLabel);
-router.get("/assigned/:userId", getCardsByAssignedUser);
+const protectedRouter = express.Router();
+protectedRouter.use(authenticate);
+
+protectedRouter.post("/", createCard);
+protectedRouter.put("/:id", updateCard);
+protectedRouter.delete("/:id", deleteCard);
+
+router.use("/", protectedRouter);
+
+// router.get("/label/:labelId", getCardsByLabel);
+// router.get("/assigned/:userId", getCardsByAssignedUser);
 
 export default router;
