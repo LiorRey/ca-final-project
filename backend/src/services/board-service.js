48be456781a47add5c6691e312a3d5aa6d915173
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-12-02 20:14:14 +0200

    feat: add an endpoint to get board with lists and cards

--- backend/src/services/board-service.js
+++ backend/src/services/board-service.js
@@ -1,6 +1,9 @@
 // backend/src/services/board-service.js
 
 import { Board } from "../models/Board.js";
+import { List } from "../models/List.js";
+import { Card } from "../models/Card.js";
+import { groupBy } from "./utils-service.js";
 
 export async function createBoard(data) {
   return await Board.create(data);
@@ -12,6 +15,30 @@ export async function getAllBoards() {
 
 export async function getBoardById(id) {
   return await Board.findById(id);
+}
+
+export async function getFullBoardById(id) {
+  const board = await Board.findById(id).lean();
+  if (!board) return null;
+
+  const [lists, cards] = await Promise.all([
+    List.find({ boardId: id }).sort({ position: 1 }).lean(),
+    Card.find({ boardId: id }).sort({ position: 1 }).lean(),
+  ]);
+
+  const cardsByListId = groupBy(cards, card => card.listId.toString());
+
+  const listsWithCards = lists.map(list => {
+    return {
+      ...list,
+      cards: cardsByListId.get(list._id.toString()) || [],
+    };
+  });
+
+  return {
+    ...board,
+    lists: listsWithCards,
+  };
 }
 
 export async function updateBoard(id, data) {
