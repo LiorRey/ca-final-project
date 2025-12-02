aadf2b31404787777cc96452453d15b14b0c7184
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-12-02 19:13:48 +0200

    refactor: throw http errors and let the error middleware handle the response

--- backend/src/middleware/authenticate.js
+++ backend/src/middleware/authenticate.js
@@ -1,13 +1,14 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/User.js";
+import createError from "http-errors";
 
-export async function authenticate(req, res, next) {
+export async function authenticate(req, _res, next) {
   const token = req.cookies.token;
-  if (!token) return res.status(401).send("Unauthorized");
+  if (!token) throw createError(401, "Unauthorized");
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
   const currentUser = await User.findById(decoded.userId);
-  if (!currentUser) return res.status(401).send("Unauthorized");
+  if (!currentUser) throw createError(401, "Unauthorized");
 
   req.currentUser = currentUser;
   next();
