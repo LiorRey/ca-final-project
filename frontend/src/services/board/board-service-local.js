c264f4e37c8f1ed6645a9c87e125f379ba8acf29
Vlad Grey <Vlad9rey@gmail.com>
2025-12-05 18:26:57 +0200

    Apply stashed changes after rebase

--- frontend/src/services/board/board-service-local.js
+++ frontend/src/services/board/board-service-local.js
@@ -423,6 +423,7 @@ export async function moveList(listId, targetBoardId, targetIndex) {
     throw error;
   }
 }
+
 export async function copyList(boardId, listId, newName) {
   try {
     const board = await getById(boardId);
