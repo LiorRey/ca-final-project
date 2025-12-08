37f7329d5e0dad3cc4d26a68d625f0f6dc5fca23
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-12-08 21:28:06 +0200

    fix: throw error if card not found to avoid getting 403 instead of 404

--- backend/src/services/permissions/card.js
+++ backend/src/services/permissions/card.js
@@ -1,16 +1,17 @@
 import { Card } from "../../models/Card.js";
 import { canModifyBoardContent, canViewBoard } from "./board.js";
+import createError from "http-errors";
 
 export async function canModifyCard(userId, cardId) {
   const card = await Card.findById(cardId);
-  if (!card) return false;
+  if (!card) throw createError(404, "Card not found");
 
   return await canModifyBoardContent(userId, card.boardId);
 }
 
 export async function canViewCard(userId, cardId) {
   const card = await Card.findById(cardId);
-  if (!card) return false;
+  if (!card) throw createError(404, "Card not found");
 
   return await canViewBoard(userId, card.boardId);
 }
