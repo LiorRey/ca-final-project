04cd00bf30ca570eef30fd88bdb35abf725d461e
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-12-09 16:06:29 +0200

    fix: temporariy fix to allow board members to edit board

--- backend/src/services/permissions/board.js
+++ backend/src/services/permissions/board.js
@@ -15,7 +15,10 @@ export async function canManageBoard(userId, boardId) {
   const board = await Board.findById(boardId);
   if (!board) return false;
 
-  return isOwner(userId, board.owner.userId);
+  return (
+    isOwner(userId, board.owner.userId) ||
+    isMemberOfArray(userId, board.members)
+  );
 }
 
 export async function canModifyBoardContent(userId, boardId) {
