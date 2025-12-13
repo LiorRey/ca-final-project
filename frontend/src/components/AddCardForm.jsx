9ea4e4062d883869466724fd569bd5d7398209ee
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-12-13 22:37:20 +0200

    fix: import the correct service dynamically

--- frontend/src/components/AddCardForm.jsx
+++ frontend/src/components/AddCardForm.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import CloseIcon from "@mui/icons-material/Close";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import { boardService } from "../services/board/board-service-local";
+import { boardService } from "../services/board";
 import { addCard } from "../store/actions/board-actions";
 
 export function AddCardForm({
