3f3b55227daa82e63621332767ea82562e9bcf58
LiorRey <lior_rey@hotmail.com>
2025-12-15 17:39:59 +0200

    feat: Const helper file for the board backgrounds

---
+++ frontend/src/services/board/board-backgrounds.js
@@ -0,0 +1,32 @@
+export const GRADIENT_BACKGROUNDS = [
+  "midnight",
+  "slate",
+  "wine",
+  "cypress",
+  "mauve",
+  "terracotta",
+  "blush",
+  "deep-teal",
+  "charcoal",
+];
+
+export const SOLID_BACKGROUNDS = [
+  "blue",
+  "orange",
+  "green",
+  "red",
+  "purple",
+  "pink",
+  "emerald",
+  "turquoise",
+  "gray",
+];
+
+export const IMAGE_BACKGROUNDS = [
+  "flower",
+  "bubble",
+  "sea",
+  "snow",
+  "volcano",
+  "crystal",
+];
